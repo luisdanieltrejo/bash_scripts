5eb34c2b6431813f86208a10d4653c5ccbed0503
luisdanieltrejo <105954075+luisdanieltrejo@users.noreply.github.com>
2022-09-24 15:17:22 -0300

    Add files via upload

---
+++ simply_check_disk_space_v1.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+while [[ i=l ]]
+do
+date
+df -h /boot | awk ' { print $5 " " $6 }'
+df -h /home | sed -n '2p' | awk '{ print $5 " " $6 }'
+df -h /var/log | awk '{ print $5 " " $6 }'
+sleep 2s
+echo
+done
